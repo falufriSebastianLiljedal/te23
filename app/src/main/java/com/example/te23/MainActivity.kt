716397e851bd32214d33a740664e0f17c1bff67c
falufriSebastianLiljedal <79262917+falufriSebastianLiljedal@users.noreply.github.com>
2025-09-04 09:02:09 +0200

    Adding first lines of code

--- app/src/main/java/com/example/te23/MainActivity.kt
+++ app/src/main/java/com/example/te23/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.te23
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var button : Button = findViewById<Button>(R.id.btn_home)
+        button.setOnClickListener {
+            ClickOnHome(button)
+        }
+
+    }
+
+    fun ClickOnHome(button : Button){
+        button.text = "I'm so clicked"
     }
 }
